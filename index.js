39c9874fe136e86c91324ebf9488a647c9271d96
GoGiants1 <69342392+GoGiants1@users.noreply.github.com>
2020-11-27 14:18:10 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+const config = require('./config/key');
+
+//applicatiom/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hyungwook:abcd1234@boilerplate.xcv0q.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() =>console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
@@ -12,8 +22,23 @@ mongoose.connect('mongodb+srv://hyungwook:abcd1234@boilerplate.xcv0q.mongodb.net
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요')
+  res.send('Hello World! 안녕하세요!!')
 })
+
+
+app.post('/register', (req,res) => {
+    //회원 가입할 때 필요한 정보들을 클라인트에서 가져오면
+    //그것들을 데이터 베이스에  넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, doc)=>{
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
