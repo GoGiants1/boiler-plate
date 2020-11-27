b3f471cbbeff3d72fb425b17fd02dea53bd75962
GoGiants1 <69342392+GoGiants1@users.noreply.github.com>
2020-11-27 20:17:41 +0900

    Auth 기능 구현

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 const {User} = require("./models/User");
-
+const { auth } = require('./middleware/auth')
 const config = require('./config/key');
 
 //applicatiom/x-www-form-urlencoded
@@ -24,11 +24,11 @@ mongoose.connect(config.mongoURI,{
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요!!!s')
+  res.send('Hello World! 안녕하세요!')
 })
 
 
-app.post('/register', (req,res) => {
+app.post('/api/users/register', (req,res) => {
     //회원 가입할 때 필요한 정보들을 클라인트에서 가져오면
     //그것들을 데이터 베이스에  넣어준다.
     const user = new User(req.body)
@@ -41,7 +41,7 @@ app.post('/register', (req,res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     //요청된 이메일을 데이터베이스에서 있는지 찾음.
     User.findOne({email : req.body.email }, (err, user) =>{
         if(!user) {
@@ -53,24 +53,34 @@ app.post('/login', (req, res) => {
         //요청된 이메일이 데이터 베이스에 있으면, 비밀번호 일치여부 확인.
         user.comparePassword(req.body.password, (err, isMatch) =>{
             if(!isMatch) return res.json({loginSuccess: false, message : "비밀번호가 틀렸습니다"})
-            
+            //비밀번호 일치하면, 토큰 생성
             user.generateToken((err, user) =>{
                 if(err) return res.status(400).send(err);
                 // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지. 여기선 쿠키 선택
                     res.cookie("x_auth", user.token)
                     .status(200)
                     .json({ loginSuccess: true, userId: user._id})
-
             })
-    
-
         })
-
     })
+})
+
+// role 1 : admin , role 2: 특정 부서 admin
+// role 0 -> 일반 유저, role 0이 아니면 관리자
 
 
-    //비밀번호 일치하면, 토큰 생성
-
+app.get('/api/users/auth',auth, (req, res) => {
+    //여기까지 미들웨어가 도착했다는 뜻은  Authentification 이 True라는 뜻
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
 })
 
 
