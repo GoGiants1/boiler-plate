f3fefdb8a54774b56eef6ae6f45c761e8a914835
GoGiants1 <69342392+GoGiants1@users.noreply.github.com>
2020-11-27 15:34:03 +0900

    backend 로그인 기능 구현

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 const {User} = require("./models/User");
 
 const config = require('./config/key');
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
@@ -22,7 +24,7 @@ mongoose.connect(config.mongoURI,{
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요!!')
+  res.send('Hello World! 안녕하세요!!!s')
 })
 
 
@@ -31,13 +33,47 @@ app.post('/register', (req,res) => {
     //그것들을 데이터 베이스에  넣어준다.
     const user = new User(req.body)
 
-    user.save((err, doc)=>{
+    user.save((err, userInfo)=>{
         if(err) return res.json({success: false, err})
         return res.status(200).json({
             success: true
         })
     })
 })
+
+app.post('/login', (req, res) => {
+    //요청된 이메일을 데이터베이스에서 있는지 찾음.
+    User.findOne({email : req.body.email }, (err, user) =>{
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+        //요청된 이메일이 데이터 베이스에 있으면, 비밀번호 일치여부 확인.
+        user.comparePassword(req.body.password, (err, isMatch) =>{
+            if(!isMatch) return res.json({loginSuccess: false, message : "비밀번호가 틀렸습니다"})
+            
+            user.generateToken((err, user) =>{
+                if(err) return res.status(400).send(err);
+                // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지. 여기선 쿠키 선택
+                    res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id})
+
+            })
+    
+
+        })
+
+    })
+
+
+    //비밀번호 일치하면, 토큰 생성
+
+})
+
+
 
 
 app.listen(port, () => {
